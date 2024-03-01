8d1b5457f899e3e00e64d180c3e3661b3d9010b1
lawalalx <lawal.alx@gmail.com>
2024-03-01 09:02:29 +0100

    Your commit message here

--- server/src/index.js
+++ server/src/index.js
@@ -3,6 +3,21 @@ const express = require('express');
 const app = express();
 const port = 3200;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+
+// Middleware to parse JSON in the request body
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+});
+
+app.post('/login', (req, res) => {
+    const user = req.body;
+    console.log(user)
+    res.send('Login successful!')
+})
+
 
 app.listen(port, () => console.log(`listening at http://localhost:${port}`));
+
+
